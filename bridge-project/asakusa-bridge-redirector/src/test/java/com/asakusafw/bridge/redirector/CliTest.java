47af7a88f3c9ee373971d4c90d16272657ade136
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2015-03-18 11:12:57 +0900

    Fix import declarations.

--- bridge-project/asakusa-bridge-redirector/src/test/java/com/asakusafw/bridge/redirector/CliTest.java
+++ bridge-project/asakusa-bridge-redirector/src/test/java/com/asakusafw/bridge/redirector/CliTest.java
@@ -15,8 +15,8 @@
  */
 package com.asakusafw.bridge.redirector;
 
-import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.FileInputStream;
