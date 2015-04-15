2bf41a32be679b8f792228f9e66283002177308f
Suguru ARAKAWA <arakawa@nautilus-technologies.com>
2015-04-15 12:16:31 +0900

    Add BuildLogParticipant.

---
+++ compiler-project/asakusa-compiler-core/src/main/java/com/asakusafw/lang/compiler/core/participant/BuildLogParticipant.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright 2011-2015 Asakusa Framework Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asakusafw.lang.compiler.core.participant;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.text.MessageFormat;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.asakusafw.lang.compiler.api.reference.BatchReference;
+import com.asakusafw.lang.compiler.common.Location;
+import com.asakusafw.lang.compiler.core.BatchCompiler.Context;
+import com.asakusafw.lang.compiler.core.basic.AbstractCompilerParticipant;
+import com.asakusafw.lang.compiler.model.graph.Batch;
+import com.asakusafw.runtime.core.context.RuntimeContext;
+
+/**
+ * Logs building environment.
+ */
+public class BuildLogParticipant extends AbstractCompilerParticipant {
+
+    static final Logger LOG = LoggerFactory.getLogger(BuildLogParticipant.class);
+
+    static final Charset ENCODING = Charset.forName("UTF-8"); //$NON-NLS-1$
+
+    /**
+     * The name prefix of the target environment variables.
+     */
+    static final String PREFIX_ENV = "ASAKUSA_"; //$NON-NLS-1$
+
+    /**
+     * The key prefix of the target system properties.
+     */
+    static final String PREFIX_SYSPROP = "com.asakusafw."; //$NON-NLS-1$
+
+    /**
+     * The output file path.
+     */
+    static final Location LOCATION = Location.of("etc/build.log"); //$NON-NLS-1$
+
+    @Override
+    public void afterBatch(Context context, Batch batch, BatchReference reference) {
+        LOG.debug("creating build log: {}", batch.getBatchId()); //$NON-NLS-1$
+        try (Editor editor = new Editor(context.getOutput().addResource(LOCATION))) {
+            perform(editor, context, batch);
+        } catch (IOException e) {
+            LOG.error(MessageFormat.format(
+                    "failed to create build log: {0}",
+                    context.getOutput().toFile(LOCATION)), e);
+        }
+    }
+
+    private void perform(Editor editor, Context context, Batch batch) {
+        // about application
+        editor.put("application.batchId", batch.getBatchId()); //$NON-NLS-1$
+        editor.put("application.batchClass", batch.getDescriptionClass().getClassName()); //$NON-NLS-1$
+
+        // about environment
+        editor.putAll("environment.", filter(System.getenv(), PREFIX_ENV)); //$NON-NLS-1$
+        editor.putAll("property.", filter(System.getProperties(), PREFIX_SYSPROP)); //$NON-NLS-1$
+
+        // about compiler
+        editor.put("compiler.buildId", context.getOptions().getBuildId()); //$NON-NLS-1$
+        editor.put("compiler.runtimeVersion", RuntimeContext.getRuntimeVersion()); //$NON-NLS-1$
+        editor.putAll("compiler.option.", context.getOptions().getProperties()); //$NON-NLS-1$
+        editor.put("compiler.dataModelProcessor", context.getTools().getDataModelProcessor()); //$NON-NLS-1$
+        editor.put("compiler.externalPortProcessor", context.getTools().getExternalPortProcessor()); //$NON-NLS-1$
+        editor.put("compiler.jobflowProcessor", context.getTools().getJobflowProcessor()); //$NON-NLS-1$
+        editor.put("compiler.batchProcessor", context.getTools().getBatchProcessor()); //$NON-NLS-1$
+        editor.put("compiler.participant", context.getTools().getParticipant()); //$NON-NLS-1$
+    }
+
+    private SortedMap<String, String> filter(Map<?, ?> map, String prefix) {
+        assert map != null;
+        assert prefix != null;
+        SortedMap<String, String> results = new TreeMap<>();
+        for (Map.Entry<?, ?> entry : map.entrySet()) {
+            String key = String.valueOf(entry.getKey());
+            if (key.startsWith(prefix)) {
+                results.put(key, String.valueOf(entry.getValue()));
+            }
+        }
+        return results;
+    }
+
+    private static class Editor implements Closeable {
+
+        private final PrintWriter writer;
+
+        public Editor(OutputStream output) {
+            writer = new PrintWriter(new OutputStreamWriter(output, ENCODING));
+        }
+
+        void put(String key, Object value) {
+            LOG.debug("build log: {} = {}", key, value); //$NON-NLS-1$
+            writer.printf("%s = %s%n", key, value); //$NON-NLS-1$
+        }
+
+        void putAll(String prefix, Map<String, String> map) {
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                put(prefix + entry.getKey(), entry.getValue());
+            }
+        }
+
+        @Override
+        public void close() throws IOException {
+            writer.close();
+        }
+    }
+}
---
+++ compiler-project/asakusa-compiler-core/src/test/java/com/asakusafw/lang/compiler/core/participant/BuildLogParticipantTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2011-2015 Asakusa Framework Team.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.asakusafw.lang.compiler.core.participant;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import com.asakusafw.lang.compiler.core.BatchCompiler;
+import com.asakusafw.lang.compiler.core.CompilerTestRoot;
+import com.asakusafw.lang.compiler.core.basic.BasicBatchCompiler;
+import com.asakusafw.lang.compiler.core.dummy.SimpleExternalPortProcessor;
+import com.asakusafw.lang.compiler.core.dummy.SimpleJobflowProcessor;
+import com.asakusafw.lang.compiler.model.graph.Batch;
+import com.asakusafw.lang.compiler.packaging.FileContainer;
+
+/**
+ * Test for {@link BuildLogParticipant}.
+ */
+public class BuildLogParticipantTest extends CompilerTestRoot {
+
+    /**
+     * simple case.
+     * @throws Exception if failed
+     */
+    @Test
+    public void simple() throws Exception {
+        jobflowProcessors.add(new SimpleJobflowProcessor());
+        externalPortProcessors.add(new SimpleExternalPortProcessor());
+        compilerParticipants.add(new BuildLogParticipant());
+
+        Batch batch = new Batch(batchInfo("testing"));
+        batch.addElement(jobflow("j0"));
+
+        FileContainer output = container();
+        BatchCompiler.Context context = new BatchCompiler.Context(context(true), output);
+        new BasicBatchCompiler().compile(context, batch);
+
+        File file = context.getOutput().toFile(BuildLogParticipant.LOCATION);
+        assertThat(file.exists(), is(true));
+    }
+}
